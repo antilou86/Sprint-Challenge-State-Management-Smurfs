0c91a58b3ed6fb4106b1bf482941b9b658474eed
antilou86 <antilou86@gmail.com>
2019-08-16 12:55:25 -0400

    WIP: added connect to app.js and created action creator functions to wire up my middleware. now, once data is in the store it should be able to flow to all anywhere it needs moving on to creating smurf display component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { getSmurfs, postSmurfs } from './actions'
 import "./App.css";
+
 class App extends Component {
+  constructor() {
+    super();
+  }
+  fetchSmurf = e => {
+    e.preventDefault();
+    this.props.getSmurfs()
+  }
+  createSmurf = e => {
+    e.preventDefault();
+    this.props.postSmurfs()
+  }
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {props.isLoading ? (<h2>LOADING ALL SMURFS...</h2>) : (<SmurfList fetchSmurf={this.fetchSmurf} createSmurf={this.createSmurf} />)}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+    isLoading: state.isLoading
+  }
+}
+
+export default connect(mapStateToProps, { getSmurfs, postSmurfs })(App)
